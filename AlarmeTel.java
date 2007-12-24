c9514bc7474bf62679f07b2ddb5091518cf1ad01
Patrice Boissier <boissier@ipgp.fr>
2007-12-24 12:23:37 +0000

    Ajout des fichiers de test de Voicent Gateway

---
+++ AlarmeTel.java
@@ -0,0 +1,47 @@
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.HttpURLConnection;
+import java.io.PrintWriter;
+import java.io.InputStream;
+import java.nio.channels.FileChannel;
+import java.io.*;
+
+
+public class AlarmeTel {
+  public static void main(String args[]) throws InterruptedException {
+
+  File listeAppel = new File("c:/temp/testctf.voc");
+  if (listeAppel.exists()) {
+    listeAppel.delete();
+  }
+
+  FileChannel in = null; // canal d'entrée
+  FileChannel out = null; // canal de sortie
+ 
+  try {
+    // Init
+    in = new FileInputStream("c:/temp/boissier.voc").getChannel();
+    out = new FileOutputStream("c:/temp/testctf.voc").getChannel();
+ 
+    // Copie depuis le in vers le out
+    in.transferTo(0, in.size(), out);
+  } catch (Exception e) {
+    e.printStackTrace(); // n'importe quelle exception
+  } finally { // finalement on ferme
+    if(in != null) {
+    	try {
+	  in.close();
+	} catch (IOException e) {}
+  }
+    if(out != null) {
+  	try {
+	  out.close();
+	} catch (IOException e) {}
+    }
+  }
+
+    Voicent voicent = new Voicent();
+
+    voicent.callTillConfirm("C:/Program Files/Voicent/BroadcastByPhone/bin/vcast.exe", "C:/temp/testctf.voc", "C:/Program Files/Voicent/MyRecordings/sample_message.wav", "1234");
+  }
+}
---
+++ CallTillConfirm Command.cmd
@@ -0,0 +1,1 @@
+vcast.exe "C:\temp\list.voc" -startnow confirmcode 1234 -wavfile "C:\Program Files\Voicent\MyRecordings\sample_message.wav" -numbers "0692703856 0692703856"
