9ec0981259f7611e9f00baa800846a502446b6c2
Patrice Boissier <boissier@ipgp.fr>
2008-03-05 11:43:23 +0000

    creation des classe CallList et WarningMessage

---
+++ src/fr/ipgp/earlywarning/telephones/CallList.java
@@ -0,0 +1,13 @@
+/**
+ * Created Mar 5, 2008 3:00:05 PM
+ * Copyright 2008 Observatoire volcanologique du Piton de La Fournaise / IPGP
+ */
+package fr.ipgp.earlywarning.telephones;
+
+/**
+ * @author Patrice Boissier
+ *
+ */
+public class CallList {
+
+}
--- src/fr/ipgp/earlywarning/triggers/Trigger.java
+++ src/fr/ipgp/earlywarning/triggers/Trigger.java
@@ -1,40 +1,120 @@
+/**
+ * Created Mar 5, 2008 3:00:05 PM
+ * Copyright 2008 Observatoire volcanologique du Piton de La Fournaise / IPGP
+ */
 package fr.ipgp.earlywarning.triggers;
 
 import java.util.*;
 import java.net.*;
+import fr.ipgp.earlywarning.telephones.*;
+import fr.ipgp.earlywarning.messages.*;
 
+/**
+ * @author Patrice Boissier
+ *
+ */
 public class Trigger {
-	private String type = null;
-	private Map<String,Object> properties = null;
+	private long id;
+	private String type;
+	private InetAddress inetAddress;
+	private String application;
+	private CallList callList;
+	private WarningMessage message;
+	private Map properties;
 	
-	public Trigger (DatagramPacket packet) {
-		this.properties = new Hashtable<String,Object>();
-		properties.put("inetAddress", new String(packet.getAddress().toString()));
-		properties.put("port", new Integer(packet.getPort()));
-		this.type = "v1"; 
-		
-		String message = new String(packet.getData(), 0, packet.getLength());
-		System.out.println(message);
+	public Trigger (long id) {
+		this.id = id;
 	}
-	
-	public String toString() {
-		String returnString = "Trigger from " + (String) this.properties.get("inetAddress") + ":" + (Integer) this.properties.get("port")+ " format "+ this.type;
-		return returnString;
+
+	/**
+	 * @return the application
+	 */
+	public String getApplication() {
+		return application;
 	}
-	
+
+	/**
+	 * @param application the application to set
+	 */
+	public void setApplication(String application) {
+		this.application = application;
+	}
+
+	/**
+	 * @return the callList
+	 */
+	public CallList getCallList() {
+		return callList;
+	}
+
+	/**
+	 * @param callList the callList to set
+	 */
+	public void setCallList(CallList callList) {
+		this.callList = callList;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @return the inetAddress
+	 */
+	public InetAddress getInetAddress() {
+		return inetAddress;
+	}
+
+	/**
+	 * @param inetAddress the inetAddress to set
+	 */
+	public void setInetAddress(InetAddress inetAddress) {
+		this.inetAddress = inetAddress;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public WarningMessage getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(WarningMessage message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the properties
+	 */
+	public Map getProperties() {
+		return properties;
+	}
+
+	/**
+	 * @param properties the properties to set
+	 */
+	public void setProperties(Map properties) {
+		this.properties = properties;
+	}
+
+	/**
+	 * @return the type
+	 */
 	public String getType() {
-		return this.type;
+		return type;
 	}
-	
+
+	/**
+	 * @param type the type to set
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
-	
-	public Object getProperty(String key) {
-		return this.properties.get(key);
-	}
-	
-	public void setProperty(String key, Object value) {
-		this.properties.put(key, value);
-	}
+
 }
