5d151c1e4c4e72e0bd708bdb2d65c8af2d1aa7e9
Patrice Boissier <boissier@ipgp.fr>
2008-03-05 11:43:23 +0000

    creation des classe CallList et WarningMessage

---
+++ java/src/fr/ipgp/earlywarning/messages/WarningMessage.java
@@ -0,0 +1,13 @@
+/**
+ * Created Mar 5, 2008 3:35:37 PM
+ * Copyright 2008 Observatoire volcanologique du Piton de La Fournaise / IPGP
+ */
+package fr.ipgp.earlywarning.messages;
+
+/**
+ * @author Patrice Boissier
+ *
+ */
+public class WarningMessage {
+
+}
