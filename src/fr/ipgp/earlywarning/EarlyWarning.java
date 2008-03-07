5050681e5420df89a2197f5d7814207a3ef65d71
Patrice Boissier <boissier@ipgp.fr>
2008-03-07 05:11:21 +0000

    Ajout du support de la journalisation (log4j)

--- src/fr/ipgp/earlywarning/EarlyWarning.java
+++ src/fr/ipgp/earlywarning/EarlyWarning.java
@@ -7,16 +7,21 @@ package fr.ipgp.earlywarning;
 import fr.ipgp.earlywarning.controler.EarlyWarningThread;
 import java.io.IOException;
 import org.apache.commons.configuration.*;
+import org.apache.log4j.*;
 
 /**
  * @author Patrice Boissier
- *
+ * Entry point for the application
+ * Reads the configuration file and then create the EarlyWarningThread.
  */
 public class EarlyWarning {
 
-	private static XMLConfiguration configuration = null;
-
+	public static XMLConfiguration configuration = null;
+	public static Logger applogger = Logger.getLogger ("EarlyWarning");
+	
 	public static void main(String[] args) throws IOException  {
+		
+		//TODO Add the logging facility
 
 		readConfiguration();
 		
@@ -48,6 +53,8 @@ public class EarlyWarning {
 			System.out.println("Fichier de configuration absent ou illisible. Fin de l'application");
 			cex.printStackTrace();
 			System.exit(1);
+			
+			//TODO Log exception
 		}
 	}
 }
