2c24e9272e2ee0aec7a299860d75a465b97cc0e2
Patrice Boissier <boissier@ipgp.fr>
2008-03-07 08:04:30 +0000

    Ajout du thread DataBaseHeartBeat

---
+++ java/src/fr/ipgp/earlywarning/controler/DataBaseHeartBeatThread.java
@@ -0,0 +1,21 @@
+/**
+ * Created Fri 07, 2008 11:40:40 AM
+ * Copyright 2008 Observatoire volcanologique du Piton de La Fournaise / IPGP
+ */
+package fr.ipgp.earlywarning.controler;
+
+/**
+ * 
+ * @author Patrice Boissier
+ *
+ */
+public class DataBaseHeartBeatThread extends Thread {
+	protected String host;
+	protected int port;
+	protected String database;
+	protected String user;
+	protected String password;
+	
+	
+
+}
