40cd5db28264e328112764380813ad4f3f0c561b
Patrice Boissier <boissier@ipgp.fr>
2008-03-13 07:28:30 +0000

    Junit test for QueueManagerThread

--- src/fr/ipgp/earlywarning/controler/QueueManagerThread.java
+++ src/fr/ipgp/earlywarning/controler/QueueManagerThread.java
@@ -4,18 +4,56 @@
  */
 package fr.ipgp.earlywarning.controler;
 
+import fr.ipgp.earlywarning.EarlyWarning;
 import fr.ipgp.earlywarning.triggers.*;
+
+import java.io.IOException;
 import java.util.*;
+
+import org.apache.commons.configuration.ConversionException;
 /**
  * Manage a trigger queue based on priorities. Launch the CallManager thread.
  * @author Patrice Boissier
  *
  */
-public class QueueManagerThread {
+public class QueueManagerThread extends Thread {
 	private Vector<Trigger> queue;
 	private int nbTriggers = 0;
 	
+    public QueueManagerThread() {
+    	this("QueueManagerThread");
+    }
+    
+    public QueueManagerThread(String name) {
+    	super(name);
+    	queue = new Vector<Trigger>();
+    }
+    
+	/**
+	 * Reorder the queue
+	 *
+	 */
 	private void sortQueue() {
 		Collections.sort(queue);
+	}
+	
+	/**
+	 * Add a trigger in the queue
+	 * @param trigger the trigger to add
+	 */
+	public void addTrigger(Trigger trigger) {
+		queue.add(trigger);
+		sortQueue();
+	}
+	
+	/**
+	 * @return queue the Queue to get
+	 */
+	public Vector<Trigger> getQueue() {
+		return queue;
+	}
+	
+	public void run() {
+    	EarlyWarning.appLogger.debug("Thread creation");
 	}
 }
