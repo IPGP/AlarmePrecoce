8fcc565f8ec64167f9ef7a09ca3d956f093fe30f
Patrice Boissier <boissier@ipgp.fr>
2008-03-13 11:55:22 +0000

    Datagram2Trigger : decode method

--- java/src/fr/ipgp/earlywarning/triggers/Datagram2Trigger.java
+++ java/src/fr/ipgp/earlywarning/triggers/Datagram2Trigger.java
@@ -7,6 +7,8 @@ package fr.ipgp.earlywarning.triggers;
 import java.util.*;
 import java.net.*;
 import fr.ipgp.earlywarning.utilities.*;
+import fr.ipgp.earlywarning.messages.*;
+import fr.ipgp.earlywarning.telephones.*;
 /**
  * @author Patrice Boissier
  *
@@ -19,30 +21,25 @@ public class Datagram2Trigger {
     
     public Datagram2Trigger(DatagramPacket packet) {
     	this.packet = packet;                
-    }
-    
-    /**
-     * Create a Trigger object from a DatagramPacket
-     * @return trigger the trigger to get
-     */
-    public Trigger getTrigger() {
     	senderAddress=packet.getAddress();
     	senderPort=packet.getPort();
     	String received = new String(packet.getData());
-    	//System.out.println("Content : " + received);
     	HashMap hashMap = decode(received);
     	trigger = new Trigger(CommonUtilities.getUniqueId(),1);
-    	//trigger.setApplication(application);
-    	//trigger.setCallList(callList);
     	trigger.setInetAddress(senderAddress);
-    	//trigger.setMessage(message);
-    	//trigger.setType(type);
-    	//trigger.setProperty(key, value);
+
+    }
+    
+    /**
+     * @return trigger the trigger to get
+     */
+    public Trigger getTrigger() {
     	return trigger;
     }
     
     /**
-     * Decode the properties of the received message from the DatagramPacket 
+     * Decode the properties of the received message from the DatagramPacket.
+     * Set the Trigger attributes.
      * @param received the received message of the DatagramPacket
      * @return hashMap the hashMap containing the 
      */
@@ -51,8 +48,12 @@ public class Datagram2Trigger {
     	String[] receivedSplit = received.split(" ");
     	
     	// Format historique OVPF : type v1
+    	// 
     	if (receivedSplit[0].equals("Sismo")) {
-    		
+    		trigger.setApplication(receivedSplit[0]);
+        	//trigger.setCallList(new TextCallList("default"));
+        	//trigger.setMessage(new TextWarningMessage(receivedSplit[3]));
+        	trigger.setType("v1");
     	}
     	
     	// Format v2
