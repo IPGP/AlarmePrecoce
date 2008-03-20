a18183f1b9972896b9e1f86e5135e58d1273a513
Patrice Boissier <boissier@ipgp.fr>
2008-03-20 13:43:17 +0000

    Finished testing DatagramTriggerConverter

--- java/test/fr/ipgp/earlywarning/triggers/TestDatagramTriggerConverter.java
+++ java/test/fr/ipgp/earlywarning/triggers/TestDatagramTriggerConverter.java
@@ -5,10 +5,12 @@ package fr.ipgp.earlywarning.triggers;
  */
 
 import org.junit.*;
+
 import java.net.*;
 import java.util.*;
-import fr.ipgp.earlywarning.triggers.*;
-
+import java.io.*;
+import fr.ipgp.earlywarning.messages.*;
+import fr.ipgp.earlywarning.telephones.*;
 /**
  * @author Patrice Boissier
  *
@@ -38,7 +40,6 @@ public class TestDatagramTriggerConverter {
 		packet.setLength(message.length());
 		DatagramTriggerConverter datagram2Trigger = new DatagramTriggerConverter(packet);
 		Trigger trigger = datagram2Trigger.getTrigger();
-		Assert.assertEquals(trigger.getInetAddress(),address);
 		try {
 			datagram2Trigger.decode(message);
 		} catch (UnknownTriggerFormatException utfe) {
@@ -51,10 +52,45 @@ public class TestDatagramTriggerConverter {
 	}
 	
 	@Test
-	public void testDecodeV1Trigger() {
+	public void testCreateV1Trigger() {
+		WarningMessage warningMessage = new TextWarningMessage("Declenchement");
+		CallList callList = new FileCallList(new File("default.csv"));
+		String message = "Sismo 13/03/2008 13:22:04 Declenchement";
+		packet.setData(message.getBytes());
+		packet.setLength(message.length());
+		DatagramTriggerConverter datagram2Trigger = new DatagramTriggerConverter(packet);
+		Trigger trig = datagram2Trigger.getTrigger();
+		try {
+			datagram2Trigger.decode(message);
+			Assert.assertEquals("Sismo",trig.getApplication());
+			//Assert.assertEquals(callList,trig.getCallList());
+			Assert.assertEquals(address,trig.getInetAddress());
+			//Assert.assertEquals(warningMessage,trig.getMessage());
+			Assert.assertEquals(1,trig.getPriority());
+			Assert.assertEquals("01",trig.getType());
+			Assert.assertEquals(true,trig.getRepeat());
+			Assert.assertEquals("13/03/2008 13:22:04",trig.getDate());
+			Assert.assertEquals("11",trig.getConfirmCode());
+			
+		} catch (UnknownTriggerFormatException utfe) {
+			System.out.println("Unknown trigger format : " + message);
+		} catch (InvalidTriggerFieldException itfe) {
+			System.out.println("Invalid trigger field : " + message);
+		} catch (MissingTriggerFieldException mtfe) {
+			System.out.println("Missing trigger field : " + message);
+		}
+	}
+	
+	@Test
+	public void testDecodeValidV1Trigger() {
 		testDecodeTrigger("Sismo 13/03/2008 13:22:04 Declenchement");
 		testDecodeTrigger("Sismo 13/03/2008 13:22:04 blalbla");
 		testDecodeTrigger("Sismo 13/03/2008 13:22:04 blalbla blablabla blabla");
+		
+	}
+
+	@Test
+	public void testDecodeInvalidV1Trigger() {	
 		// Invalid time
 		testDecodeTrigger("Sismo 13/03/2008 56:22:04 sdf erffs");
 		testDecodeTrigger("Sismo 13/03/2008 56:22:04 sdf");
@@ -69,35 +105,41 @@ public class TestDatagramTriggerConverter {
 	}
 	
 	@Test
-	public void testDecodeV2Trigger() {
-		// vv p yyyy/MM/dd HH:mm:ss application calllist repeat message
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true |Ceci est un message d'alerte!|");
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856 true |Ceci est un message 		d'alerte.|");
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 fichier.csv true |Ceci est un message, d'alerte?|");
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 fichier.csv true |Alerté|");
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true message.wav");
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true warningMessage.wav");
+	public void testDecodeValidV2Trigger() {
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true 12 |Ceci est un message d'alerte!|");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856 true 1 |Ceci est un message 		d'alerte.|");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 fichier.csv true 123542 |Ceci est un message, d'alerte?|");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 fichier.csv true 2263 |Alerté|");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true 958 message.wav");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true 58547 warningMessage.wav");
+	}
+
+	@Test
+	public void testDecodeInvalidV2Trigger() {
 		// Invalid priority
-		testDecodeTrigger("02 1d 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true warningMessage.wav");
+		testDecodeTrigger("02 1d 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true 11 warningMessage.wav");
 		// Invalid type
-		testDecodeTrigger("020 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true Ceci est un message d alerte");
+		testDecodeTrigger("020 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true 11 Ceci est un message d alerte");
 		// Incomplete message
 		testDecodeTrigger("02 1 0000/03/18 13:22:04 ");
 		// Invalid date
-		testDecodeTrigger("02 1 2008/13/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true |Ceci est un message d alerte|");
-		testDecodeTrigger("02 1 2008/03/32 13:22:04 appli_dataTaker01 0692703856,06924555455 true |Ceci est un message d alerte|");
+		testDecodeTrigger("02 1 2008/13/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true 11 |Ceci est un message d alerte|");
+		testDecodeTrigger("02 1 2008/03/32 13:22:04 appli_dataTaker01 0692703856,06924555455 true 11 |Ceci est un message d alerte|");
 		// Invalid time
-		testDecodeTrigger("02 1 2008/12/18 25:22:04 appli_dataTaker01 0692703856,06924555455 true |Ceci est un message d alerte|");
+		testDecodeTrigger("02 1 2008/12/18 25:22:04 appli_dataTaker01 0692703856,06924555455 true 11 |Ceci est un message d alerte|");
 		// Invalid warning message
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true |Ceci est un $message d alerte|");
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true |Ceci est un message d alerte");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true 11 |Ceci est un $message d alerte|");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true 11 |Ceci est un message d alerte");
 		// Invalid text call list
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455,toto true |Ceci est un message d alerte|");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455,toto true 11 |Ceci est un message d alerte|");
 		// Invalid file call list
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 fichier.csvd true |Ceci est un message d alerte|");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 fichier.csvd true 11 |Ceci est un message d alerte|");
 		// Invalid text
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true message.fsd");
-		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true warningMessage.wav coucou");		
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 0692703856,06924555455 true 11 message.fsd");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true 11 warningMessage.wav coucou");
+		// Invalid confirm code
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true 5854705 warningMessage.wav");
+		testDecodeTrigger("02 1 2008/03/18 13:22:04 appli_dataTaker01 file_test.csv true 585z5 warningMessage.wav");
 	}
 		
 	public static junit.framework.Test suite() {
