b6bb34404dd0d7f264009a5a702aff2acb8c2d3f
Patrice Boissier <boissier@ipgp.fr>
2008-03-21 18:42:36 +0000

    Problem with regex on warning message : works with junit but not in production...

--- java/src/fr/ipgp/earlywarning/triggers/DatagramTriggerConverter.java
+++ java/src/fr/ipgp/earlywarning/triggers/DatagramTriggerConverter.java
@@ -6,6 +6,7 @@ package fr.ipgp.earlywarning.triggers;
 
 import java.net.*;
 import java.io.*;
+import java.util.regex.*;
 import fr.ipgp.earlywarning.utilities.*;
 import fr.ipgp.earlywarning.messages.*;
 import fr.ipgp.earlywarning.telephones.*;
@@ -18,13 +19,13 @@ public class DatagramTriggerConverter implements TriggerConverter {
     protected InetAddress senderAddress;
     protected int senderPort;
     protected Trigger trigger;
-    protected String received;
+    protected String packetContent;
     
     public DatagramTriggerConverter(DatagramPacket packet) {
     	this.packet = packet;                
     	this.senderAddress=packet.getAddress();
     	this.senderPort=packet.getPort();
-    	this.received = new String(packet.getData());
+    	this.packetContent = new String(packet.getData());
     	this.trigger = new Trigger(CommonUtilities.getUniqueId(),1);
     	this.trigger.setInetAddress(senderAddress);
     }
@@ -43,29 +44,29 @@ public class DatagramTriggerConverter implements TriggerConverter {
      * @throws UnknownTriggerFormatException, InvalidTriggerFieldException, MissingTriggerFieldException
      */
     public void decode() throws UnknownTriggerFormatException, InvalidTriggerFieldException, MissingTriggerFieldException {
-    	String[] receivedSplit = received.split(" ");
+    	String[] packetContentSplit = this.packetContent.split(" ");
     	int version;
     	
-    	if (receivedSplit[0].matches("\\d\\d"))
-    		version = Integer.parseInt(receivedSplit[0]);
+    	if (packetContentSplit[0].matches("\\d\\d"))
+    		version = Integer.parseInt(packetContentSplit[0]);
     	else {
-    		if(receivedSplit[0].equals("Sismo")) 
+    		if(packetContentSplit[0].equals("Sismo")) 
     			version = 1;
     		else {
-    			throw new UnknownTriggerFormatException("Unknown version : " + receivedSplit[0]);
+    			throw new UnknownTriggerFormatException("Unknown version : " + packetContentSplit[0]);
     		}
     	}
     	
     	switch(version)
         {
             case 1:
-                decodeV1(receivedSplit);
+                decodeV1(packetContentSplit);
                 break;
             case 2:
-                decodeV2(receivedSplit);
+                decodeV2(packetContentSplit);
                 break;
             default:
-            	throw new UnknownTriggerFormatException("Unknown version : " + receivedSplit[0]);
+            	throw new UnknownTriggerFormatException("Unknown version : " + packetContentSplit[0]);
         }
     }
     
@@ -75,19 +76,19 @@ public class DatagramTriggerConverter implements TriggerConverter {
      * @param elements the elements of the received message 
      * @throws InvalidTriggerFieldException
      */
-    private void decodeV1(String[] elements) throws InvalidTriggerFieldException{
-    	if (CommonUtilities.isDate(elements[1] + " " + elements[2], "dd/MM/yyyy HH:mm:ss") 
-    			&& elements.length > 3) {
-    		trigger.setApplication(elements[0]);
+    private void decodeV1(String[] packetContentElements) throws InvalidTriggerFieldException{
+    	if (CommonUtilities.isDate(packetContentElements[1] + " " + packetContentElements[2], "dd/MM/yyyy HH:mm:ss") 
+    			&& packetContentElements.length > 3) {
+    		trigger.setApplication(packetContentElements[0]);
     		trigger.setCallList(new FileCallList(new File("default.csv")));
-	    	trigger.setMessage(new TextWarningMessage(elements[3]));
+	    	trigger.setMessage(new TextWarningMessage(packetContentElements[3]));
 	    	trigger.setType("01");
-	    	trigger.setDate(elements[1] + " " + elements[2]);
+	    	trigger.setDate(packetContentElements[1] + " " + packetContentElements[2]);
 	    	trigger.setRepeat(true);
 	    	trigger.setConfirmCode("11");
 	    	System.out.println("valid v1 format");
     	} else 
-        	throw new InvalidTriggerFieldException ("Invalid V1 trigger fields : " + received);  		
+        	throw new InvalidTriggerFieldException ("Invalid V1 trigger fields : " + this.packetContent);  		
     }
     
     /**
@@ -105,74 +106,52 @@ public class DatagramTriggerConverter implements TriggerConverter {
      * @param elements the elements of the received message
      * @return true if the decoding was successful else false
      */
-    private void decodeV2(String[] elements) throws InvalidTriggerFieldException, MissingTriggerFieldException {
-    	String message = new String();
+    private void decodeV2(String[] packetContentElements) throws InvalidTriggerFieldException, MissingTriggerFieldException {
+    	String warningMessage = new String();
     	boolean first = true;
-    	if (elements.length>8) {
-    		for (int j=8; j<elements.length; j++) {
+    	if (packetContentElements.length>8) {
+    		for (int j=8; j<packetContentElements.length; j++) {
     			if (first) {
-    				message += elements[j];
+    				warningMessage = packetContentElements[j];
     				first = false;
     			} else
-    				message += " " + elements[j];
+    				warningMessage = warningMessage + " " + packetContentElements[j];
     		}
     	} else 
-    		throw new MissingTriggerFieldException ("Not enough fields for a V2 trigger : " + received);
-		
-    	//TEST CODE
+    		throw new MissingTriggerFieldException ("Not enough fields for a V2 trigger : " + this.packetContent);
     	
-//    	for (int j=0; j<8 ; j++) {
-//    		System.out.println("Elements " + j + " : " + elements[j]);
-//    	}
-//    	
-//    	System.out.println("Message : " + message);
-//    	
-//    	if (elements[1].matches("\\d")) {
-//    		System.out.println("Element 1 OK");
-//    	}
-//    	if (CommonUtilities.isDate(elements[2] + " " + elements[3], "yyyy/MM/dd HH:mm:ss")) {
-//    		System.out.println("Element 2 et 3 OK");
-//    	}
-//    	if (elements[4].matches("\\w*")) {
-//    		System.out.println("Element 4 OK");
-//    	}
-//    	if (elements[6].equals("true") || elements[6].equals("false")) {
-//    		System.out.println("Element 5 OK");
-//    	}
-//    	if (elements[7].matches("\\d+") && elements[7].length() < 7) {
-//    		System.out.println("Element 6 OK");
-//    	}
-//    	if (message.matches("\\w+\\.wav") ) {
-//    		System.out.println("Element 7 OK");
-//    	}
-    	
-    	
-    	// END TEST CODE
-    	
-    	if (elements[1].matches("\\d")
-    			&& CommonUtilities.isDate(elements[2] + " " + elements[3], "yyyy/MM/dd HH:mm:ss")
-    			&& elements[4].matches("\\w*")
-    			&& (elements[5].matches("\\w+\\.csv") || elements[5].matches("(\\d*)(,\\d*)*"))
-    			&& (elements[6].equals("true") || elements[6].equals("false"))
-    			&& elements[7].matches("\\d+") && elements[7].length() < 7 
-    			&& (message.matches("\\w+\\.wav") || message.matches("\\|[\\w\\s!\\?,\\.'\\u00C0-\\u00FF]*\\|"))
-    			){
-    		if (elements[5].matches("\\w+\\.csv"))
-    			trigger.setCallList(new FileCallList(new File(elements[5])));
+    	if (!packetContentElements[1].matches("\\d"))
+    		throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + packetContentElements[1]);
+    	if (!CommonUtilities.isDate(packetContentElements[2] + " " + packetContentElements[3], "yyyy/MM/dd HH:mm:ss"))
+    		throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + packetContentElements[2] + " " + packetContentElements[3]);
+    	if (!packetContentElements[4].matches("\\w*"))
+    		throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + packetContentElements[4]);
+    	if (!(packetContentElements[6].equals("true") || packetContentElements[6].equals("false")))
+    		throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + packetContentElements[6]);
+    	if (!packetContentElements[7].matches("\\d+") || packetContentElements[7].length() > 7)
+    		throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + packetContentElements[7]);
+    	if (packetContentElements[5].matches("\\w+\\.csv"))
+    		trigger.setCallList(new FileCallList(new File(packetContentElements[5])));
+    	else {
+    		if (packetContentElements[5].matches("(\\d*)(,\\d*)*"))
+    			trigger.setCallList(new TextCallList(packetContentElements[5]));
     		else
-    			trigger.setCallList(new TextCallList(elements[5]));
-    		if (message.matches("\\w+\\.wav"))
-    			trigger.setMessage(new FileWarningMessage(new File(message)));
+    			throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + packetContentElements[5]);
+    	}
+    	if (warningMessage.matches("\\w+\\.wav"))
+    		trigger.setMessage(new FileWarningMessage(new File(warningMessage)));
+    	else {
+    		if (warningMessage.matches("\\|[\\w\\s!\\?,\\.'\\u00C0-\\u00FF]*\\|"))
+    			trigger.setMessage(new TextWarningMessage(warningMessage));
     		else
-    			trigger.setMessage(new TextWarningMessage(message));
-    		trigger.setApplication(elements[4]);
-	    	trigger.setType(elements[0]);
-	    	trigger.setPriority(Integer.parseInt(elements[1]));
-	    	trigger.setDate(elements[2] + " " + elements[3]);
-	    	trigger.setRepeat(Boolean.parseBoolean(elements[6]));
-			trigger.setConfirmCode(elements[7]);
-	    	System.out.println("valid v2 format");
-    	} else
-    		throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + received);
+    			throw new InvalidTriggerFieldException ("Invalid V2 trigger field(s) : " + warningMessage);
+    	}
+    	trigger.setApplication(packetContentElements[4]);
+    	trigger.setType(packetContentElements[0]);
+    	trigger.setPriority(Integer.parseInt(packetContentElements[1]));
+    	trigger.setDate(packetContentElements[2] + " " + packetContentElements[3]);
+    	trigger.setRepeat(Boolean.parseBoolean(packetContentElements[6]));
+    	trigger.setConfirmCode(packetContentElements[7]);
+    	System.out.println("valid v2 format");
     }
 }
