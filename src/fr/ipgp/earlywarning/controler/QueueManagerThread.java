6f70ed0cb1450513b6ff6eea492c2a0e4ae4dbb5
Patrice Boissier <boissier@ipgp.fr>
2008-03-28 17:45:54 +0000

    Implemented PriorityBlockingQueue for the QueueManagerThread

--- src/fr/ipgp/earlywarning/controler/QueueManagerThread.java
+++ src/fr/ipgp/earlywarning/controler/QueueManagerThread.java
@@ -7,16 +7,14 @@ package fr.ipgp.earlywarning.controler;
 import fr.ipgp.earlywarning.EarlyWarning;
 import fr.ipgp.earlywarning.telephones.PhoneCall;
 import fr.ipgp.earlywarning.triggers.*;
-import java.util.*;
-
+import java.util.concurrent.PriorityBlockingQueue;
 /**
  * Manage a trigger queue based on priorities. Launch the CallManager thread.
  * @author Patrice Boissier
  *
  */
 public class QueueManagerThread extends Thread {
-	private Vector<Trigger> queue;
-	private int nbTriggers = 0;
+	private PriorityBlockingQueue<Trigger> queue;
     protected boolean moreTriggers = true;
 	
     public QueueManagerThread() {
@@ -25,25 +23,15 @@ public class QueueManagerThread extends Thread {
     
     public QueueManagerThread(String name) {
     	super(name);
-    	queue = new Vector<Trigger>();
+    	queue = new PriorityBlockingQueue<Trigger>();
     }
-    
-	/**
-	 * Reorder the queue
-	 *
-	 */
-	private void sortQueue() {
-		Collections.sort(queue);
-	}
-	
+    	
 	/**
 	 * Add a trigger in the queue
 	 * @param trigger the trigger to add
 	 */
 	public void addTrigger(Trigger trigger) {
 		queue.add(trigger);
-		nbTriggers++;
-		sortQueue();
 	}
 	
 	/**
@@ -63,7 +51,7 @@ public class QueueManagerThread extends Thread {
 	/**
 	 * @return queue the Queue to get
 	 */
-	public Vector<Trigger> getQueue() {
+	public PriorityBlockingQueue<Trigger> getQueue() {
 		return queue;
 	}
 	
@@ -71,14 +59,14 @@ public class QueueManagerThread extends Thread {
 	 * @return a string representing the queue manager 
 	 */
 	public String toString() {
-		return nbTriggers + " Triggers : " + queue.toString();
+		return queue.size() + " Triggers : " + queue.toString();
 	}
 	
 	public void run() {
     	EarlyWarning.appLogger.debug("Thread creation");
 		PhoneCall phoneCall = new PhoneCall();
     	while (moreTriggers) {
-    		if (nbTriggers > 0) {
+    		if (queue.size() > 0) {
     			if (phoneCall.isCallInProgress()) {
     	    		try {
     					Thread.sleep(5000);
@@ -87,9 +75,7 @@ public class QueueManagerThread extends Thread {
     					EarlyWarning.appLogger.error("Error while sleeping!");
     				}    				
     			} else {
-    				phoneCall.setTrigger(queue.firstElement());
-    				queue.remove(0);
-    				nbTriggers--;
+    				phoneCall.setTrigger(queue.poll());
     				phoneCall.callTillConfirm();
     			}
     		} else {
