6f6b27624a2f77f3c15dc837a824363f6e0bd363
Patrice Boissier <boissier@ipgp.fr>
2008-04-17 06:34:30 +0000

    Gateway support implemented

---
+++ java/test/fr/ipgp/earlywarning/gateway/TestTestGateway.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package fr.ipgp.earlywarning.gateway;
+
+import org.junit.*;
+/**
+ * @author patriceboissier
+ *
+ */
+public class TestTestGateway {
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+
+	}
+	
+	@Test
+	public void testCreateTestGateway() {
+		//TestGateway testGateway = 
+	}
+	
+	public static junit.framework.Test suite() {
+		return new junit.framework.JUnit4TestAdapter(TestTestGateway.class);
+	}
+}
