f78bc07653d540a776db5e4db93cb95ff6eb1a22
Patrice Boissier <boissier@ipgp.fr>
2008-04-30 12:39:46 +0000

    Began implementing UI and Observer pattern for the CallLists

--- src/fr/ipgp/earlywarning/gateway/VoicentGateway.java
+++ src/fr/ipgp/earlywarning/gateway/VoicentGateway.java
@@ -202,6 +202,43 @@ public class VoicentGateway implements Gateway{
 		}
 	}
 	
+    public void callTillConfirm(String vcastexe, String vocfile, String wavfile, String ccode)
+    {
+        // call request url
+        String urlstr = "/ocall/callreqHandler.jsp";
+
+        // setting the http post string
+        String poststr = "info=";
+        poststr += URLEncoder.encode("Simple Call till Confirm");
+
+        poststr += "&phoneno=1111111"; // any number
+
+        poststr += "&firstocc=10";
+        poststr += "&selfdelete=0";
+
+        poststr += "&startexec=";
+        poststr += URLEncoder.encode(vcastexe);
+
+        String cmdline = "\"";
+        cmdline += vocfile;
+        cmdline += "\"";
+        cmdline += " -startnow";
+        cmdline += " -confirmcode ";
+        cmdline += ccode;
+        cmdline += " -wavfile ";
+        cmdline += "\"";
+        cmdline += wavfile;
+        cmdline += "\"";
+
+        // add -cleanstatus if necessary
+
+        poststr += "&cmdline=";
+        poststr += URLEncoder.encode(cmdline);
+
+        // Send like a Call Request
+        postToGateway(urlstr, poststr);
+    }
+	
 	public String callTillConfirm(String vcastexe, String vocFile, String waveFile, String confirmCode, String [] phoneNumbers) {
 		try {
 			
@@ -219,7 +256,7 @@ public class VoicentGateway implements Gateway{
 		    String urlString = "/ocall/callreqHandler.jsp";
 		    String postString = "info=" + URLEncoder.encode("Simple Call till Confirm", encoding);
 		    postString += "&phoneno=666";
-		    postString += "&firstocc=50";
+		    postString += "&firstocc=10"; // the call can happen any time between now and 10 minutes later. After 10 minutes, the gateway will issue a "Too late to call" error message.
 		    postString += "&selfdelete=0";
 		    postString += "&startexec=" + URLEncoder.encode(vcastexe, encoding);
 		
