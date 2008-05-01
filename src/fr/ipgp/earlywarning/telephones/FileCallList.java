41c0da04c6780845284245257e09d89be3b27e35
Patrice Boissier <boissier@ipgp.fr>
2008-05-01 17:18:24 +0000

    Refactoring class and method names

--- src/fr/ipgp/earlywarning/telephones/FileCallList.java
+++ src/fr/ipgp/earlywarning/telephones/FileCallList.java
@@ -4,47 +4,77 @@
 package fr.ipgp.earlywarning.telephones;
 
 import javax.swing.event.EventListenerList;
+import java.io.*;
 /**
  * @author patriceboissier
  *
  */
 public class FileCallList implements CallList {
-	private String file;
+	private String fileName;
+	private File file;
 	private String type;
 	private EventListenerList listeners;
 	
-	public FileCallList(String file) throws InvalidFileNameException {
-		String [] fileElements = file.split("\\.");
-		if (fileElements.length < 2)
-			throw new InvalidFileNameException("Invalid File Name : " + file);
-		if ((!fileElements[fileElements.length-1].equals("txt")) && (!fileElements[fileElements.length-1].equals("voc")))
-			throw new InvalidFileNameException("Invalid File Name : " + file);
-		this.file = file;
-		this.type = fileElements[fileElements.length-1];
+	public FileCallList(String fileName) throws InvalidFileNameException {
+		this.fileName = fileName;
+		this.file = new File(fileName);
+		this.type = extractExtension(fileName);
 		listeners = new EventListenerList();
+	}
+
+	public FileCallList(File file) throws InvalidFileNameException {
+		this.file = file;
+		this.fileName = file.getName();
+		this.type = extractExtension(fileName);
+		listeners = new EventListenerList();
+	}
+	
+	private String extractExtension(String fileName) throws InvalidFileNameException {
+		String [] fileElements = fileName.split("\\.");
+		if (fileElements.length < 2)
+			throw new InvalidFileNameException("Invalid File Name : " + fileName);
+		if ((!fileElements[fileElements.length-1].equals("txt")) && (!fileElements[fileElements.length-1].equals("voc")))
+			throw new InvalidFileNameException("Invalid File Name : " + fileName);
+		return fileElements[fileElements.length-1];
 	}
 	
 	/**
 	 * @return a String representing the object
 	 */
-	public String toString() {
-		String result = file.toString();
-		return result;
-	}
+//	public String toString() {
+//		String result = file.toString();
+//		return result;
+//	}
 	
 	/**
 	 * 
 	 */
-	public void setFile(String file) {
-		this.file = file;
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+		this.file = new File(fileName);
+		//this.type = extractExtension(fileName);
 		fireFileChanged();
 	}
 	
 	/**
 	 * @return the file to get
 	 */
-	public String getFile() {
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @return the file
+	 */
+	public File getFile() {
 		return file;
+	}
+
+	/**
+	 * @param file the file to set
+	 */
+	public void setFile(File file) {
+		this.file = file;
 	}
 
 	/**
@@ -73,7 +103,7 @@ public class FileCallList implements CallList {
 		FileCallListListener[] listenerList = (FileCallListListener[])listeners.getListeners(FileCallListListener.class);
 		
 		for(FileCallListListener listener : listenerList) {
-			listener.fileReferenceCallListChanged(new FileCallListChangedEvent(this, getFile()));
+			listener.fileReferenceCallListChanged(new FileCallListChangedEvent(this, getFileName()));
 		}
 	}
 }
