5cbeeb0cabaa4e1fbe5fad9746351c1c305da84d
Patrice Boissier <boissier@ipgp.fr>
2008-05-05 17:24:43 +0000

    Mailer implementation

--- java/src/fr/ipgp/earlywarning/utilities/Mailer.java
+++ java/src/fr/ipgp/earlywarning/utilities/Mailer.java
@@ -11,30 +11,64 @@ import java.util.*;
  * @author patriceboissier
  */
 public class Mailer {
+	private static Mailer uniqueInstance;
 	private String host;
 	private String from;
+	private String username;
+	private String password;
 	private Properties properties;
 	
-	public Mailer(String host, String from) {
+	private Mailer(String host, String from, String username, String password) {	
 		this.host = host;
 		this.from = from;
+		this.username = username;
+		this.password = password;
 		properties = System.getProperties();
 		properties.put("mail.smtp.host", this.host);
 	}
 
+	public static synchronized Mailer getInstance(String host, String from, String username, String password) {
+    	if (uniqueInstance == null) {
+    		uniqueInstance = new Mailer(host, from, username, password);
+    	}
+    	return uniqueInstance;
+    }
+	
 	public void sendNotification(String to, String subject, String body) throws MessagingException {
+	    String protocol = "smtp";
+	    properties.put("mail." + protocol + ".auth", "true");
 		Session session = Session.getDefaultInstance(properties, null);
 		MimeMessage message = new MimeMessage(session);
 		message.setFrom(new InternetAddress(from));
 		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-		message.setSubject("Hello JavaMail");
-		message.setText("Welcome to JavaMail");
-		Transport.send(message);
+		message.setSubject(subject);
+		message.setText(body);
+
+	    Transport transport = session.getTransport(protocol);
+	    try {
+	        transport.connect(username, password);
+	        transport.sendMessage(message, message.getAllRecipients());
+	    } finally {
+	        transport.close();
+	    }
 	}
 	
-	public void sendNotifications(String[] to, String subject, String body) throws MessagingException {
-		for (String dest : to) {
-			sendNotification(dest,subject,body);
-		}
+	public void sendNotifications(String[] tos, String subject, String body) throws MessagingException {
+	    String protocol = "smtp";
+	    properties.put("mail." + protocol + ".auth", "true");
+		Session session = Session.getDefaultInstance(properties, null);
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));
+//		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		message.setSubject(subject);
+		message.setText(body);
+		message.saveChanges();
+	    Transport transport = session.getTransport(protocol);
+	    transport.connect(username, password);
+	    for(String to : tos) {
+	    	Address internetAddress = new InternetAddress(to);
+	    	transport.sendMessage(message, new Address[] { internetAddress });
+	    }	    
+	    transport.close();
 	}
 }
