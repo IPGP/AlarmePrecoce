ae3cf2888c6827e80ce26091206eafac838102ec
Thomas Kowalski <thom.kowa@gmail.com>
2018-07-27 12:23:18 +0400

    Restructuration des dossiers (séparation EarlyWarning / TriggerSender)

--- TriggerSender/src/main/java/fr/ipgp/earlywarning/triggersender/Main.java
+++ TriggerSender/src/main/java/fr/ipgp/earlywarning/triggersender/Main.java
@@ -5,11 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
-    private static void print(String s)
-    {
-        System.out.println(s);
-    }
-
     public static void main(final String args[]) throws IOException {
         File f = new File(".");
         System.out.println(f.getCanonicalPath());
@@ -31,28 +26,18 @@ public class Main {
         String configurationFile;
     }
 
-    private static Options parseArgs(final String args[])
-    {
+    private static Options parseArgs(final String args[]) {
         Options options = new Options();
-        for(String arg : args)
-        {
+        for (String arg : args) {
             if (arg.startsWith("--file="))
                 options.configurationFile = arg.split("=")[1];
         }
 
-        if (options.configurationFile == null)
-        {
+        if (options.configurationFile == null) {
             System.err.println("Missing parameter --file");
             System.exit(-1);
         }
 
         return options;
     }
-
-    private static void applyConfiguration()
-    {
-
-    }
-
-
 }
